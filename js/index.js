feeb45eee549bb84bd87365876c5362d690f3773
vinsend2 <vinsend2@gmail.com>
2021-11-11 08:29:47 +0500

    commit1431

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 "use strict";
-
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = function (callback, thisArg) {
+      thisArg = thisArg || window;
+      for (var i = 0; i < this.length; i++) {
+          callback.call(thisArg, this[i], i, this);
+      }
+  };
+}
 
 const $ = (x, parent = document) => {
   return parent.querySelector(x);
--- js/script.js
+++ js/script.js
@@ -10,7 +10,14 @@ ScrollReveal().reveal(".wwd-card", {
 });
 
 "use strict";
-
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = function (callback, thisArg) {
+      thisArg = thisArg || window;
+      for (var i = 0; i < this.length; i++) {
+          callback.call(thisArg, this[i], i, this);
+      }
+  };
+}
 
 const $ = (x, parent = document) => {
   return parent.querySelector(x);
